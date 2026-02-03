86ff87c96f964ac360c0632e9a93071fc6b2bcf9
“anjali” <anjalisharmag10@gmail.com>
2026-02-03 23:26:13 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import React from "react"
-
+import { RouterProvider } from "react-router-dom"  
+import Router from "./Router"
 import "./Styles/Global.css"
 
 
 function App() {
   return (
-    <>
+
     <Router/>
-    </>
+    
   )
 }
 
